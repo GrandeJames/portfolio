05958a9d80b5beafda9cbd5fcbd2687117274c81
James Louie Grande <jlgrande@hawaii.edu>
2024-01-08 22:43:12 -1000

    Update animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <motion.h1
-        className="my-12 text-6xl md:text-8xl lg:text-9xl font-bold tracking-widest"
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.4 }}
+        className="my-12 text-6xl md:text-8xl lg:text-[10rem] font-bold tracking-widest"
+        initial={{ opacity: 0, filter: "blur(5px)" }}
+        animate={{ opacity: 1, filter: "blur(0px)" }}
+        transition={{ duration: 0.5 }}
       >
         JAMES
       </motion.h1>
@@ -21,7 +21,7 @@ export default function Home() {
           className="flex flex-col items-center"
           initial={{ opacity: 0, y: 300 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4 }}
+          transition={{ duration: 0.5 }}
         >
           <p className="text-gray-800 mb-5 max-w-[50ch] text-center">
             👋 Hi, I&apos;m a full-stack developer and a Computer Science
@@ -29,12 +29,30 @@ export default function Home() {
             about creating maintainable and scalable software solutions that
             customers love.
           </p>
+        </motion.div>
+      </div>
+      <div className="overflow-hidden">
+        <motion.div
+          className="flex flex-col items-center"
+          initial={{ opacity: 0, y: 300 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <div className="m-3 text-center">
             Email:{" "}
             <a href="mailto:james.grande6@gmail.com" className="underline">
               james.grande6@gmail.com
             </a>
           </div>
+        </motion.div>
+      </div>
+      <div className="overflow-hidden">
+        <motion.div
+          className="flex flex-col items-center"
+          initial={{ opacity: 0, y: 300 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <div className="flex gap-5">
             <Link href={"https://www.linkedin.com/in/jlag6/"}>
               <LinkedinIcon />
