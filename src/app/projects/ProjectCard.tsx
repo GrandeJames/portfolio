eaef7193fc2b4967d67ac67a62ae68d443b8f4d7
James Louie Grande <jlgrande@hawaii.edu>
2024-03-31 10:56:39 -1000

    Refactor icon

--- src/app/projects/ProjectCard.tsx
+++ src/app/projects/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Tools } from "../../components/Tools";
 import Image from "next/image";
+import { RightArrowIcon } from "../icons/RightArrowIcon";
 
 interface ProjectCardProps {
   title: string;
@@ -40,36 +41,12 @@ export const ProjectCard = ({
       <div className="flex gap-10 my-3 font-medium">
         {githubLink && (
           <Link href={githubLink} target="_blank" className="flex gap-1">
-            GitHub{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            GitHub <RightArrowIcon />
           </Link>
         )}
         {websiteLink && (
           <Link href={websiteLink} target="_blank" className="flex gap-1">
-            Website{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            Website <RightArrowIcon />
           </Link>
         )}
       </div>
