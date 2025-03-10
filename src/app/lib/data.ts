b57122afeeb7dc05e509b551f35e0d712f40d17b
James Louie Grande <jlgrande@hawaii.edu>
2025-03-10 01:27:26 -1000

    Update project description

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Bryzi",
     description:
-      "Bryzi is a full-stack web application designed specifically for students. It enables them to rapidly generate tasks from course schedule images (commonly found in course syllabi) to drastically reduce the time spent on manual task entry. Additionally, Bryzi offers a feature to automatically plan their daily tasks, eliminating manual planning and rescheduling.",
+      "Bryzi is an automated task management full-stack web application designed specifically for students. It enables them to rapidly generate tasks from course schedule images (commonly found in course syllabi) to drastically reduce the time spent on manual task entry. Additionally, Bryzi can automatically plan daily tasks, eliminating manual planning and rescheduling.",
     tools: ["TypeScript", "NextJS", "TailwindCSS", "Google Gemini API", "Supabase", "React"],
     websiteLink: "https://bryzi.com/",
   },
