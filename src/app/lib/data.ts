be0061e5b80bd75b81b30b8aa45a59f969f72cbc
James Louie Grande <jlgrande@hawaii.edu>
2025-05-01 20:36:33 -1000

    Update info

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,15 +1,15 @@
 export const projects = [
   {
-    title: "Bryzi",
+    title: "Bryzi - AI Task Manager for Students",
     description:
-      "Bryzi is an automated task management full-stack web application designed specifically for students. It uses GenAI to rapidly generate tasks in seconds from course schedule images (commonly found in course syllabi) to drastically reduce the time spent on manual task entry. Additionally, Bryzi will be able to automatically plan daily tasks, eliminating manual planning and rescheduling.",
+      "Bryzi is an automated task management full-stack web application designed specifically for students. It uses GenAI to rapidly generate tasks in seconds from course schedule images to drastically reduce the time spent on manual task entry.",
     tools: ["TypeScript", "NextJS", "TailwindCSS", "Google Gemini API", "Supabase", "React"],
     websiteLink: "https://bryzi.com/",
     githubLink: "https://github.com/GrandeJames/bryzi",
     imageSrc: "/bryzi.webp",
   },
   {
-    title: "Makai",
+    title: "Makai - Marine Debris Reporting Platform",
     description:
       "Won 3rd place at the 2023 Hawaii Annual Code Challenge. Makai is a web app used to help with the removal and data tracking on large marine debris in Hawai'i.",
     tools: ["JavaScript", "React", "MongoDB", "TailwindCSS"],
@@ -18,7 +18,7 @@ export const projects = [
     githubLink: "https://github.com/HACC-2023/makai",
   },
   {
-    title: "Office Space Manager",
+    title: "OSMICS - UH Manoa ICS Office Space Manager",
     description:
       "A web application for tracking office space usage and assets for the ICS department at the University of Hawai'i at Mānoa.",
     tools: ["JavaScript", "React", "D3.js", "Bootstrap", "Meteor"],
